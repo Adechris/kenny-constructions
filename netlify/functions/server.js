079cc384809ae324da413c80a2e71732691f7e9f
Your Name <you@example.com>
2026-06-14 10:34:55 +0100

    Fix Netlify function imports

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,6 +1,15 @@
-import serverHandler from "../../server";
+let serverHandler;
+
+async function getServerHandler() {
+  if (!serverHandler) {
+    serverHandler = (await import("../../dist/server/index")).default || (await import("../../dist/server/index")).handler;
+  }
+  return serverHandler;
+}
 
 export async function handler(event, context) {
+  const handler = await getServerHandler();
+  
   const request = new Request(
     event.rawUrl || `https://${event.headers.host || "localhost"}${event.path || "/"}`,
     {
@@ -10,7 +19,7 @@ export async function handler(event, context) {
     }
   );
 
-  const response = await serverHandler.fetch(request, {}, {});
+  const response = await handler.fetch(request, {}, {});
 
   return {
     statusCode: response.status,
